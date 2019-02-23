9e7656e35c4347dcfc95653264528c1e54f11c02
Fernando Reyes Bejarano <fernandorb_8@hotmail.com>
2019-02-23 00:38:37 -0500

    Tarea completa

--- JuegoDeMemoriaSemana2.playground/Contents.swift
+++ JuegoDeMemoriaSemana2.playground/Contents.swift
@@ -1,5 +1,13 @@
 for i in 0 ... 100 {
+    if i % 5 == 0 {
+        print("\(i)Bingo!!!")
+    }
     if i >= 30 && i <= 40 {
         print("\(i)Viva Swift!!!")
+    }
+    if i % 2 == 0 {
+        print("\(i)par!!!")
+    } else {
+        print("\(i)impar!!!")
     }
 }
